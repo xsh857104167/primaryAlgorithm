72a1dff5db3560c0283425a15a85a7ca900b5e23
xsh857104167 <xsh857104167@163.com>
2021-07-19 17:44:38 +0800

    没做完“旋转图像”。

---
+++ ArrayTest/src/RotateImage.java
@@ -0,0 +1,33 @@
+import org.junit.Test;
+
+/**
+ * @author Murphy Xu
+ * @create 2021-07-19 16:32
+ */
+public class RotateImage {
+    /**
+     * 先转置，再调整行列号
+     * @param matrix
+     */
+    public void rotate(int[][] matrix){
+
+    }
+
+    /**
+     * 转置
+     * @param matrix
+     */
+    public void transposition(int[][] matrix){
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+
+            }
+        }
+    }
+
+
+    @Test
+    public void test(){
+
+    }
+}
