110d6ecd1a381974bc4ee468ef3f845894bf5d7e
xsh857104167 <xsh857104167@163.com>
2021-07-21 19:46:18 +0800

    做完“实现strStr”的四种方法，包括KMP。

--- ArrayTest/src/string/MyStrStr.java
+++ ArrayTest/src/string/MyStrStr.java
@@ -40,19 +40,91 @@ public class MyStrStr {
     }
 
     /**
-     *
+     * 别人的逐个判断
+     * 1777ms,50.1%;38.6MB,9.64%
      * @param haystack
      * @param needle
      * @return
      */
     public int strStr2(String haystack, String needle){
-        return 0;
+        if (needle.length() == 0)
+            return 0;
+        int i = 0;
+        int j = 0;
+        while (i < haystack.length() && j< needle.length()){
+            if (haystack.charAt(i) == needle.charAt(j)){
+                ++i;
+                ++j;
+            } else {
+                i = i -j +1;
+                j = 0;
+            }
+            if (j == needle.length())
+                return i - j;
+        }
+        return -1;
     }
+
+    /**
+     * 截取字符串比较
+     * 386ms,7.45%;.8.8MB,5.10%
+     * @param haystack
+     * @param needle
+     * @return
+     */
+    public int strStr3(String haystack, String needle){
+        int length = needle.length();
+        int total = haystack.length() - length +1;
+        for (int start = 0; start < total; start++) {
+            if (haystack.substring(start, start + length).equals(needle)){
+                return start;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * KMP方法
+     * 5ms,32.27%;38.5MB,19.84%
+     * @param haystack
+     * @param needle
+     * @return
+     */
+    public int strStr4(String haystack, String needle){
+        int n = haystack.length();
+        int m = needle.length();
+        if (m == 0){
+            return 0;
+        }
+        int[] pi = new int[m];
+        for (int i = 1, j = 0; i < m; i++){
+            while (j > 0 && needle.charAt(i) != needle.charAt(j)){
+                j = pi[j-1];
+            }
+            if (needle.charAt(i) == needle.charAt(j)){
+                j++;
+            }
+            pi[i] = j;
+        }
+        for (int i = 0, j = 0; i < n; i++){
+            while (j>0 && haystack.charAt(i) != needle.charAt(j)){
+                j = pi[j-1];
+            }
+            if (haystack.charAt(i) == needle.charAt(j)){
+                j++;
+            }
+            if (j==m){
+                return i - m + 1;
+            }
+        }
+        return -1;
+    }
+
 
     @Test
     public void test(){
-        String haystack = "hello";
-        String needle = "ll";
-        System.out.println(strStr(haystack, needle));
+        String haystack = "";
+        String needle = "";
+        System.out.println(strStr3(haystack, needle));
     }
 }
