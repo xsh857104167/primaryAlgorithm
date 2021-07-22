039bd7a89b10d88be57e55a24a8a2584da46079a
xsh857104167 <xsh857104167@163.com>
2021-07-22 10:17:38 +0800

    做完”删除链表的倒数第N个节点“的三种方法“。

--- ArrayTest/src/list/RemoverNthFromEnd.java
+++ ArrayTest/src/list/RemoverNthFromEnd.java
@@ -11,7 +11,8 @@ public class RemoverNthFromEnd {
      * 因为是让你返回一个链表的head;
      * 所以明显可以再创建一个新的
      * 或者交换位置也可以。<br>
-     * 最简单的方法是先数一下那个是倒数第nge，再删，需要两次遍历
+     * 最简单的方法是先数一下那个是倒数第n个，再删，需要两次遍历
+     * 0ms,100%;36.6MB,18.18%
      * @param head
      * @param n
      * @return
@@ -23,14 +24,65 @@ public class RemoverNthFromEnd {
             count++;
             p = p.next;
         }
-//        System.out.println(count);
+
         p = head;
+        if (count == n){
+            head = p.next;
+            return head;
+        }
         for (int i = 0; i < (count - n -1); ++i){
             p = p.next;
         }
-        // 找到后
+
         p.next = p.next.next;
         return head;
+    }
+
+    /**
+     * 双指针+假头
+     * 0ms,100%;36.5MB,29.31%
+     * @param head
+     * @param n
+     * @return
+     */
+    public ListNode removeNthFromEnd2(ListNode head, int n) {
+        ListNode top = new ListNode(0, head);
+        ListNode slow = top, fast = head;
+        for (int i = 1; i < n; i++) {
+            fast = fast.next;
+        }
+
+        while (fast.next != null){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        slow.next = slow.next.next;
+        return top.next;
+    }
+
+    /**
+     * 递归
+     * 0ms, 100%;36.5MB,30.11%
+     * @param head
+     * @param n
+     * @return
+     */
+    public ListNode removeNthFromEnd3(ListNode head, int n) {
+        int pos = length(head, n);
+        if (pos == n)
+            return head.next;
+
+        return head;
+    }
+
+    public int length(ListNode node, int n){
+        if (node == null)
+            return 0;
+        int pos = length(node.next, n) + 1;
+        if (pos == n + 1)
+            node.next = node.next.next;
+
+        return pos;
     }
 
     @Test
@@ -43,7 +95,7 @@ public class RemoverNthFromEnd {
             point.next = tmp;
             point = point.next;
         }
-        head = removeNthFromEnd(head, 2);
+        head = removeNthFromEnd2(head, 2);
         point = head;
         while(point != null){
             System.out.println(point.val);
