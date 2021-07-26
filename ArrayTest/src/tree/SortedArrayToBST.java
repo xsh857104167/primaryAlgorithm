04ad1bcc62fb9d0507af852a3743cc1e604c8112
xsh857104167 <xsh857104167@163.com>
2021-07-26 22:25:25 +0800

    做完“将有序数组转换为二叉搜索树”的一种方法。

---
+++ ArrayTest/src/tree/SortedArrayToBST.java
@@ -0,0 +1,36 @@
+package tree;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * @author Murphy Xu
+ * @create 2021-07-26 21:27
+ */
+public class SortedArrayToBST {
+
+    /**
+     * 0ms, 100%; 37.5MB,99.78%
+     * @param nums
+     * @return
+     */
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if (nums.length == 0)
+            return null;
+        if (nums.length == 1)
+            return new TreeNode(nums[0]);
+
+        TreeNode root = new TreeNode(nums[nums.length/2]);
+        root.left = sortedArrayToBST(Arrays.copyOfRange(nums, 0, nums.length/2));
+        root.right = sortedArrayToBST(Arrays.copyOfRange(nums, nums.length/2 + 1, nums.length));
+
+        return root;
+    }
+
+    @Test
+    public void test(){
+        int[] nums = {-10,-3,0,5,9};
+        sortedArrayToBST(nums);
+    }
+}
