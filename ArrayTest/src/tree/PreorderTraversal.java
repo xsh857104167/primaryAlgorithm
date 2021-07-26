407a51add419d7662b40cfe48ed682df3c45c674
xsh857104167 <xsh857104167@163.com>
2021-07-26 11:31:58 +0800

    做完补充前序遍历二叉树的三种方法。

--- ArrayTest/src/tree/PreorderTraversal.java
+++ ArrayTest/src/tree/PreorderTraversal.java
@@ -1,6 +1,8 @@
 package tree;
 
 import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -11,6 +13,7 @@ public class PreorderTraversal {
 
     /**
      * 递归
+     * 0ms,36.8MB
      * @param root
      * @return
      */
@@ -29,4 +32,66 @@ public class PreorderTraversal {
         preorder(root.right, res);
     }
 
+    /**
+     * 非递归
+     * 0ms, 100%; 36.3MB,98.28%
+     * @param root
+     * @return
+     */
+    public List<Integer> preorderTraversal2(TreeNode root){
+        List<Integer> res = new ArrayList<>();
+        if (root == null)
+            return res;
+
+        Deque<TreeNode> stack = new LinkedList<>();
+        TreeNode node = root;
+
+        while (node != null || !stack.isEmpty()){
+            while (node != null){
+                res.add(node.val);
+                stack.push(node);
+                node = node.left;
+            }
+
+            node = stack.pop();
+            node = node.right;
+
+        }
+        return res;
+    }
+
+    /**
+     * Morris
+     * 0ms, 100%;36.4MB, 95.26%
+     * @param root
+     * @return
+     */
+    public List<Integer> preorderTraversal3(TreeNode root){
+        List<Integer> res = new ArrayList<>();
+        if (root == null)
+            return res;
+        TreeNode p1 = root, p2 = null;
+
+        while (p1 != null){
+            p2 = p1.left;
+            if (p2 != null){
+                while (p2.right != null && p2.right != p1){
+                    p2 = p2.right;
+                }
+
+                if (p2.right == null){
+                    res.add(p1.val);
+                    p2.right = p1;
+                    p1 = p1.left;
+                    continue;
+                }else{
+                    p2.right = null;
+                }
+            } else {
+                res.add(p1.val);
+            }
+            p1 = p1.right;
+        }
+        return res;
+    }
 }
