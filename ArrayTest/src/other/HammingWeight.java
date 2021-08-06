9e775c8b3144c49aa229bca51a16b8e93795a7cd
xsh857104167 <xsh857104167@163.com>
2021-08-06 15:51:42 +0800

    做完“位1的个数”的两种方法。

--- ArrayTest/src/other/HammingWeight.java
+++ ArrayTest/src/other/HammingWeight.java
@@ -8,6 +8,12 @@ import org.junit.Test;
  */
 public class HammingWeight {
     // you need to treat n as an unsigned value
+
+    /**
+     * 0ms, 100%; 35.2MB, 75.58%
+     * @param n
+     * @return
+     */
     public int hammingWeight(int n) {
         int count = 0;
         while (n != 0){
@@ -17,11 +23,23 @@ public class HammingWeight {
         return count;
     }
 
+    /**
+     * 每次消去最右边的1，直到消完为止
+     * 0ms，100%; 35.4MB, 31.24%
+     * @param n
+     * @return
+     */
+    public int hammingWeight2(int n) {
+        int count = 0;
+        while (n != 0){
+            n = n & (n-1);
+            count ++;
+        }
+        return count;
+    }
+
     @Test
     public void test(){
-        int n = -5;
-        n =  n >>> 1;
 
-        System.out.println(n);
     }
 }
